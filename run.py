046cd9ca6be7300d9e3e837f602b9aecb61464f2
ShonaOB <shona-obriendev@outlook.com>
2022-04-26 14:47:16 +0000

    Add runtime file for pushing to heroku

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("For example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
         validate_data(sales_data)
@@ -80,7 +80,6 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-             
     return surplus_data
 
 
@@ -152,4 +151,3 @@ def main():
 
 print("Welcome to Love Sandwiches Data Automation \n")
 main()
-
