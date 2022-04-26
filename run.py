16c5c7be61fec92a41d3cf2ad60648004257d066
ShonaOB <shona-obriendev@outlook.com>
2022-04-26 09:52:24 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -55,16 +55,16 @@ def validate_data(values):
 
 
 
-
+"""
 def update_sales_worksheet(data):
-    """
+    
     update sales worksheet add new row with the list of data provided
-    """
+
     print('Updating sales worksheet.../n')
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print('sales worksheet updated successfully \n')
-
+    """
 def calculate_surplus_data(sales_row):
     """
     compare sales with stock and calculate surplus. 
@@ -83,15 +83,25 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+"""
+def update_surplus_worksheet(data):
 
-def update_surplus_worksheet(new_surplus_data):
-    """
     update the surplus worksheet with the calculated surplus numbers
-    """
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(new_surplus_data)
+    surplus_worksheet.append_row(data)
     print('surplus worksheet updated successfully')
+"""
+def update_worksheet(data, worksheet):
+    """
+    receives a list of integers to be inserted into a worksheet
+    update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 
 def main():
     """
@@ -99,9 +109,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
 print("Welcome to Love Sandwiches Data Automation \n")
 main()
